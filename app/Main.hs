846d6f83f724a95fc6dcd65d66ce5beff74b00db
Marg Jacqualine <forwmrty@gmail.com>
2021-07-18 19:51:13 +0300

    made my first HTTP request! (getUpdates), saved request body to file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
-import Lib
+import qualified Data.ByteString.Char8 as BC
+import           Network.HTTP.Simple
+
+token :: BC.ByteString
+token = "1913597879:AAEQ8hYhCyNoavFzxHWYcf2Lg-ejOSt48NU"
+
+getUpdatesRequest :: Request
+getUpdatesRequest =
+  setRequestMethod "GET" $
+  setRequestSecure True $
+  "https://api.telegram.org/bot1913597879:AAEQ8hYhCyNoavFzxHWYcf2Lg-ejOSt48NU/getUpdates"
 
 main :: IO ()
-main = someFunc
+main = do
+  response <- httpBS $ getUpdatesRequest
+  let code = getResponseStatusCode response
+  putStrLn $ mconcat ["Response code is ", show code]
+  if code == 200
+    then do
+      let body = getResponseBody response
+      putStrLn "writing to file..."
+      BC.writeFile "updates.json" body
+    else putStrLn "request failed with error"
